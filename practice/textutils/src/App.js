5367a86ef20ce50ff8b887ed631bbcb3ff14b40c
Shrikant Jamale <shrikantj0794@gmail.com>
2023-07-25 15:03:28 +0530

    Practice set added

---
+++ practice/textutils/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './compoenent/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar title="shrikant" about="about us"/>
+      {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
