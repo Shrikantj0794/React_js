fd2b84fc7a194af14df0469ba9b7ef4a3f54dd18
Shrikant Jamale <shrikantj0794@gmail.com>
2023-07-26 13:35:51 +0530

    Update State

--- practice/textutils/src/compoenent/Form.js
+++ practice/textutils/src/compoenent/Form.js
@@ -8,21 +8,22 @@ export default function Form(props) {
 
   const handleUpClick = ()=>{
     console.log("converting")
-    setText("you have to clicked on converter case")
+    let newText = text.toUpperCase();
+    setText(newText)
   }
   const handleonChange = (event)=>{
     console.log("on change")
     setText(event.target.value)
   }
   
-
     return (
     <div>
         <h3>{props.heading}</h3>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleonChange} id="myForm" rows="8"></textarea>
-            <button className='btn btn-primary' onClick={handleUpClick}>convert to UpperCase</button>
-        </div> 
+            <button className='btn btn-primary' onClick={ handleUpClick}>convert to UpperCase</button> 
+            {/* Call multiple functions onClick ReactJS */}
+            </div>  
     </div>  
   )
 }
