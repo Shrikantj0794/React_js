5a46ed03103c86a52add01449df202eb85e347e4
Shrikant Jamale <shrikantj0794@gmail.com>
2023-07-26 14:19:38 +0530

    Adding more Logic to TextUtils

--- practice/textutils/src/compoenent/Form.js
+++ practice/textutils/src/compoenent/Form.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 export default function Form(props) {
-  const [text, setText] = useState('enter the text---')
+  const [text, setText] = useState('')
   // text = "new text" -- Wrong way to change text
   // setText = "new text" -- wright way to change text value
  
@@ -15,15 +15,25 @@ export default function Form(props) {
     console.log("on change")
     setText(event.target.value)
   }
-  
+  const changelowercase = ()=>{
+    let lowertext =text.toLowerCase();
+    setText(lowertext)
+  }
     return (
     <div>
         <h3>{props.heading}</h3>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleonChange} id="myForm" rows="8"></textarea>
-            <button className='btn btn-primary' onClick={ handleUpClick}>convert to UpperCase</button> 
+            <button className='btn btn-primary mx-2' onClick={ handleUpClick}>convert to UpperCase</button>
+            <button className='btn btn-primary mx-2' onClick={ changelowercase}>convert to LowerCase</button> 
             {/* Call multiple functions onClick ReactJS */}
-            </div>  
+          </div>
+        <div className="container mb-3">
+          words {text.split(" ").length} and character {text.length}
+          <p>{0.008 * text.split(" ").length} minutes read</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
+          </div>  
     </div>  
   )
 }
