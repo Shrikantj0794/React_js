c05efa86c2c63907c77c136d62d9006127d3a277
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-31 13:25:55 +0530

    hello world

---
+++ helloworld/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'; //providing you a jsx in the file.
+import ReactDOM from 'react-dom' // enable to use of DOM-specific methods
+
+ReactDOM.render(<h1> Hello World</h1>, document.getElementById('root'));
